e982c38fd1190e424a882c50c35f04f76ffa31bd
BhanuKiranChaluvadi <nthdimensionalreality@gmail.com>
2017-10-10 18:10:48 -0600

    Added implementation for constructor and implemented function called subscriberCallback

--- include/husky_highlevel_controller/HuskyHighlevelController.hpp
+++ include/husky_highlevel_controller/HuskyHighlevelController.hpp
@@ -8,16 +8,25 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
 
 namespace husky_highlevel_controller {
 
 class HuskyHighlevelController {
 public:
+
 	HuskyHighlevelController(ros::NodeHandle& nodeHandle);
+
 	virtual ~HuskyHighlevelController();
+
+	void subscriberCallback(const sensor_msgs::LaserScan::ConstPtr& laserScan);
+
+	void subscribe();
+
 
 private:
 	ros::NodeHandle nodeHandle_;
+	ros::Subscriber subscriber_;
 };
 
 } /* name space husky_highlevel_controller */
--- src/HuskyHighlevelController.cpp
+++ src/HuskyHighlevelController.cpp
@@ -6,18 +6,30 @@
  */
 
 #include "husky_highlevel_controller/HuskyHighlevelController.hpp"
+#include <algorithm>
 
 namespace husky_highlevel_controller {
 
 HuskyHighlevelController::HuskyHighlevelController(ros::NodeHandle& nodeHandle): nodeHandle_(nodeHandle)
 {
 	// TODO Auto-generated constructor stub
+	subscriber_ = nodeHandle.subscribe("/scan", 10, &HuskyHighlevelController::subscriberCallback, this);
 
 }
 
 HuskyHighlevelController::~HuskyHighlevelController()
 {
 	// TODO Auto-generated destructor stub
+}
+
+void HuskyHighlevelController::subscriberCallback(const sensor_msgs::LaserScan::ConstPtr& laserScan)
+{
+
+//	Note that unbounded arrays in ROS msgs are mapped onto std::vector in C++.
+	float smallestValue;
+	std::vector<float> laserScanData = laserScan->ranges;
+	smallestValue = *std::min_element(laserScanData.begin(), laserScanData.end());
+	ROS_INFO(" Smallest distance measurement :[%f]", smallestValue);
 }
 
 } /* name space husky_highlevel_controller */
